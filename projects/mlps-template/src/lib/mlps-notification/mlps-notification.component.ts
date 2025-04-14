220ca09aa59c21fda1ff28a4708e7a5c5c49ece5
DESKTOP-ODU9K7N\Edynet-Penta <ermes.diprisco@penta-si.it>
2025-04-14 16:12:15 +0200

    eliminati z-index modale (da testare) aggiunti altri componenti

--- projects/mlps-template/src/lib/mlps-notification/mlps-notification.component.ts
+++ projects/mlps-template/src/lib/mlps-notification/mlps-notification.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'mlps-notification',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './mlps-notification.component.html',
   styleUrl: './mlps-notification.component.scss'
 })
 export class MLPSNotificationComponent {
+  condition: boolean = true
+  @Input() listaNotifiche!: any[];
+  gestioneLetturaNotifiche: boolean = true;
 
+  segnaTutteNotificheLette() {
+    }
+
+    testoNotificheToggle(idNotifica: number, idUtente:number, titoloNotifica: string, testo: string, isLetto: boolean) {
+    }
 }
